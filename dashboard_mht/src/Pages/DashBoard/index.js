a4a916f67854876b5f9a9361fe79dce825f22518
Kinghuend <minh.huynh@madison-technology.com>
2023-06-08 01:40:14 +0700

    up Card of DashBoard

--- dashboard_mht/src/Pages/DashBoard/index.js
+++ dashboard_mht/src/Pages/DashBoard/index.js
@@ -1,9 +1,31 @@
-import { Typography } from "antd";
+import { Card, Space, Statistic, Typography } from "antd";
+import {SiToptal, SiVirustotal} from "react-icons/si";
+import {GiMoneyStack} from "react-icons/gi";
+import { FaDollarSign } from "react-icons/fa";
+import { BsPersonBadge } from "react-icons/bs";
 function DashBoard() {
   return (
     <div>
       <Typography.Title level={4}>Dashboard</Typography.Title>
+      <Space direction="horizontal">
+        <DashBoardCard icon = {<SiVirustotal/>} title={"Total Staff"} value = {100}/>
+        <DashBoardCard icon = {<FaDollarSign/>} title={"Salary Total"} value = {100}/>
+        <DashBoardCard icon = {<SiToptal/>} title={"Total Staff1"} value = {100}/>
+        <DashBoardCard icon = {<BsPersonBadge/>} title={"Total Staff2"} value = {100}/>
+      </Space>
+
     </div>
   );
+}
+function DashBoardCard({icon, title, value}) {
+  return (
+    <Card>
+    <Space direction="horizontal">
+      {icon}
+      <Statistic title={title} value={value}/>
+    </Space>
+  </Card>
+  )
+  
 }
 export default DashBoard;
