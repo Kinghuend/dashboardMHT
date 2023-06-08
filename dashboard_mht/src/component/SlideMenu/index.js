a27ecd5be9563f0428e82f98dc3f59c258bc0b92
Kinghuend <minh.huynh@madison-technology.com>
2023-06-08 21:36:08 +0700

    lỗi staffMenu

--- dashboard_mht/src/component/SlideMenu/index.js
+++ dashboard_mht/src/component/SlideMenu/index.js
@@ -1,9 +1,11 @@
-import { Menu } from "antd"
+
+import { Menu,Dropdown } from "antd"
 import { MdDashboard } from 'react-icons/md';
 import { BsPeopleFill } from 'react-icons/bs';
 import { FaPeopleCarry} from 'react-icons/fa';
 import { MdPersonalVideo} from 'react-icons/md';
 import { useNavigate } from "react-router-dom";
+import { DownOutlined, SmileOutlined } from '@ant-design/icons';
 // import {
 //     MdDashboard,
 //     BsPeopleFill,
@@ -11,37 +13,89 @@ import { useNavigate } from "react-router-dom";
 //     MdPersonalVideo,
 // } from "@ant-design/icons";
 function SlideMenu() {
-    const navigate = useNavigate()
-    return(
-        <div className="SlideMenu">
-            <Menu
-            onClick={(item)=>{
-                //item.key
-                navigate(item.key);
-            }}
-            items={[
-                {
-                    label: "DashBoard",
-                    icon: <MdDashboard/>,
-                    key: '/',
-                },
-                {
-                    label: "Staff",
-                    icon: <BsPeopleFill/>,
-                    key: '/Staff',
-                },
-                {
-                    label: "Employee",
-                    icon: <FaPeopleCarry/>,
-                    key: '/Employee',
-                },
-                {
-                    label: "Personal",
-                    icon: <MdPersonalVideo/>,
-                    key: '/Personal',
-                }
-            ]}></Menu>
-        </div>
-    )
-}
+    const navigate = useNavigate();
+  
+    const item = [
+        {
+          key: '1',
+          label: (
+            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+              1st menu item
+            </a>
+          ),
+        },
+        {
+          key: '2',
+          label: (
+            <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+              2nd menu item (disabled)
+            </a>
+          ),
+          icon: <SmileOutlined />,
+          disabled: true,
+        },
+        {
+          key: '3',
+          label: (
+            <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+              3rd menu item (disabled)
+            </a>
+          ),
+          disabled: true,
+        },
+        {
+          key: '4',
+          danger: true,
+          label: 'a danger item',
+        },
+      ];
+    // staffMenu
+    const staffMenu = () => (
+        <Dropdown
+          menu={{
+            item,
+          }}
+        >
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              Hover me
+              <DownOutlined />
+            </Space>
+          </a>
+        </Dropdown>
+      );
+
+  
+    return (
+      <div className="SlideMenu">
+        <Menu
+          items={[
+            {
+              label: "DashBoard",
+              icon: <MdDashboard />,
+              key: '/',
+            },
+            {
+              label: "Staff",
+              icon: <BsPeopleFill />,
+              key: '/Staff',
+              submenu: staffMenu, // Thêm dropdown menu vào mục "Staff"
+            },
+            {
+              label: "Employee",
+              icon: <FaPeopleCarry />,
+              key: '/Employee',
+            },
+            {
+              label: "Personal",
+              icon: <MdPersonalVideo />,
+              key: '/Personal',
+            }
+          ]}
+        />
+      </div>
+    );
+  }
+  
+
 export default SlideMenu
