433140c572400d119848c54b0138f07df80ded75
Kinghuend <minh.huynh@madison-technology.com>
2023-06-08 13:56:09 +0700

    Up table

--- dashboard_mht/src/Pages/DashBoard/index.js
+++ dashboard_mht/src/Pages/DashBoard/index.js
@@ -1,18 +1,28 @@
-import { Card, Space, Statistic, Typography } from "antd";
+import { Card, Space, Statistic, Table, Typography } from "antd";
 import {SiToptal, SiVirustotal} from "react-icons/si";
 import {GiMoneyStack} from "react-icons/gi";
 import { FaDollarSign } from "react-icons/fa";
 import { BsPersonBadge } from "react-icons/bs";
+import { useEffect } from "react";
+import { useState } from 'react';
+import { getCustomers, getInventory, getOrders, getRevenue } from "../../API";
+
 function DashBoard() {
   return (
     <div>
-      <Typography.Title level={4}>Dashboard</Typography.Title>
-      <Space direction="horizontal">
-        <DashBoardCard icon = {<SiVirustotal/>} title={"Total Staff"} value = {100}/>
-        <DashBoardCard icon = {<FaDollarSign/>} title={"Salary Total"} value = {100}/>
-        <DashBoardCard icon = {<SiToptal/>} title={"Total Staff1"} value = {100}/>
-        <DashBoardCard icon = {<BsPersonBadge/>} title={"Total Staff2"} value = {100}/>
+      <Space size={20} direction="vertical">
+        <Typography.Title level={4}>Dashboard</Typography.Title>
+        <Space direction="horizontal">
+          <DashBoardCard icon = {<SiVirustotal/>} title={"Total Staff"} value = {100}/>
+          <DashBoardCard icon = {<FaDollarSign/>} title={"Salary Total"} value = {100}/>
+          <DashBoardCard icon = {<SiToptal/>} title={"Total Staff1"} value = {100}/>
+          <DashBoardCard icon = {<BsPersonBadge/>} title={"Total Staff2"} value = {100}/>
+        </Space>
+        <Space>
+          <RecentOrder/>
+        </Space>
       </Space>
+
 
     </div>
   );
@@ -25,7 +35,40 @@ function DashBoardCard({icon, title, value}) {
       <Statistic title={title} value={value}/>
     </Space>
   </Card>
+  ) 
+}
+function RecentOrder() {
+  const [dataSource, setDataSource] = useState()
+  const [loading, setLoading] = useState(false)
+  useEffect(() =>{
+    setLoading(true)
+    getOrders().then(res=>{
+      setDataSource(res.products.splice(0,3))
+      setLoading(false)
+    })
+  },[])
+  return(
+  <>
+  <Typography.Text>Recent Order</Typography.Text>
+  <Table columns={[
+    {
+      title: "Title",
+      dataIndex :"title"
+    },
+    {
+      title: "Quantity",
+      dataIndex :"quantity"
+    },
+    {
+      title: "Price",
+      dataIndex :"discountedPrice"
+    },
+  ]}
+  loading={loading}
+  dataSource={dataSource}
+  pagination={false}
+  ></Table>
+  </>
   )
-  
 }
 export default DashBoard;
